69b75d35d3e68458684df4b0a03fdc90a97cb69d
Zapperdax <qa80986@gmail.com>
2022-12-16 12:12:02 +0500

    Styling Chatbot Page

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,6 +1,13 @@
 import React from "react";
+import InputEmoji from "react-input-emoji";
 
 const Chatbot = () => {
+  const [text, setText] = React.useState("");
+  const [chat, setChat] = React.useState([]);
+
+  const handleText = (text) => {
+    setChat((preValue) => [...preValue, text]);
+  };
   return (
     <div className="chatbotPage">
       <img
@@ -13,8 +20,27 @@ const Chatbot = () => {
           <h1>Welcome, User</h1>
           <h4>How May I Assist You</h4>
         </div>
+        <div className="chatMain">
+          {chat.map((message, i) => {
+            return (
+              <div key={i} className="userMessage">
+                <p>{message}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="chatMessageSend">
+          <InputEmoji
+            value={text}
+            onChange={setText}
+            cleanOnEnter
+            onEnter={handleText}
+            placeholder="Chat"
+            borderColor="#1A1A1A"
+            height={300}
+          />
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };

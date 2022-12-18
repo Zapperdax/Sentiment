4d2de02fc310e8447efe27c5638618ddb6ec2a89
Zapperdax <qa80986@gmail.com>
2022-12-18 15:37:34 +0500

    Chatbotlandingpage tweaks

--- src/components/ChatbotLandingPage.js
+++ src/components/ChatbotLandingPage.js
@@ -12,13 +12,23 @@ const ChatbotLandingPage = () => {
       <div className="infoContainer">
         <div className="chatLeftSide">
           <h1>Make it easy to express your feelings</h1>
-          <div className="chatButtonContainer">
+          <div
+            style={{ margin: "1rem 0 2rem" }}
+            className="chatButtonContainer"
+          >
             <Button variant="text">Learn More</Button>
             <Button onClick={handleClick} variant="contained">
               Get Started
             </Button>
           </div>
-          <p style={{ marginBottom: "0.5rem" }}>Your Personal Mood Companion</p>
+          <p
+            style={{
+              padding: "1rem",
+              color: "white",
+            }}
+          >
+            Your Personal Mood Companion
+          </p>
           <div className="chatleftparagraphcontainer">
             <p className="chatleftparagraph">Insert Links</p>
             <p className="chatleftparagraph">Insert Links</p>

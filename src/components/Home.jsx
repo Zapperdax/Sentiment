50daff6d83b533e018e8442c761d6c483cd264cf
Zapperdax <qa80986@gmail.com>
2022-12-29 18:06:25 +0500

    Setting up front camera to take pictures

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useCallback, useRef} from "react";
 import { Button, Stack, Typography, useMediaQuery } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Webcam from 'react-webcam';
 import ai from "../assets/ai.png";
 
 const Home = () => {
   const is700 = useMediaQuery("(max-width:700px)");
   const isMobile = useMediaQuery("(max-width:530px)");
   const isTab = useMediaQuery("(max-width:1050px)");
+  const [isCameraOn, setIsCameraOn] = React.useState(false);
+  const [img, setImg] = React.useState(null);
+  const webcamRef = useRef(null);
   const navigate = useNavigate();
+
+  const videoConstraints = {
+    width: { min: 1280 },
+    height: { min: 720 },
+    facingMode: "user",
+  };
+
+  const capture = useCallback(()=> {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+  }, [webcamRef]);
+  
   const handleScan = () =>{
-    navigate('/facedetection');
+    setIsCameraOn(true);
   }
 
+  console.log(img);
   return (
     <Stack
       direction={isTab ? "column" : "row"}
@@ -21,6 +38,36 @@ const Home = () => {
         gap: isTab ? "3rem" : "4rem",
       }}
     >
+      {isCameraOn && img === null ?
+        <Stack sx={styles.webcamStyles}>
+          <Webcam ref={webcamRef} imageSmoothing={true} screenshotFormat='image/jpeg' mirrored={true} videoConstraints={videoConstraints} />
+          <Button
+            sx={{
+              fontSize: isMobile ? "12px" : is700 ? "16px" : '18px' ,
+              lineHeight: isMobile ? "22px" : is700 ? "24px" : '28px',
+              ...styles.stackBtn,
+            }}
+            onClick={capture}
+          >
+            {!isMobile ? 'Capture Emotion' : "Capture"}
+          </Button>
+        </Stack>
+        :
+        img === null ? null :
+        <Stack sx={styles.webcamStyles}>
+        <img src={img} alt='emotion' />
+        <Button
+          sx={{
+            fontSize: isMobile ? "12px" : is700 ? "16px" : '18px' ,
+            lineHeight: isMobile ? "22px" : is700 ? "24px" : '28px',
+            ...styles.stackBtn,
+          }}
+          onClick={()=> setImg(null)}
+        >
+          {!isMobile ? 'Retake Photo' : "Retake"}
+        </Button>
+      </Stack>}
+        
       <Stack
         flex={1}
         gap={isTab ? "2rem" : "1.3rem"}
@@ -126,6 +173,14 @@ const styles = {
     justifyContent: "center",
     alignItems: "center",
   },
+  webcamStyles: {
+   position: 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+   maxWidth: '90%',
+   zIndex: '1'
+  }
 };
 
 export default Home;

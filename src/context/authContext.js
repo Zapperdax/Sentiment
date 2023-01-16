b7ca07cc70c6fb496d9dc5dc215188862e1f8d6d
Mujtaba-Abbasi <gmabbsi92@gmail.com>
2023-01-16 23:16:48 +0500

    create authContext

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,24 +1,32 @@
-import React, {useReducer, createContext} from 'react';
+/** @format */
+
+import { useReducer, createContext } from "react";
 
 export const AuthContext = createContext();
 
-export const authReducer = useReducer((state, action)=>{
-    switch(action){
-        case 'LOGIN':
-            return{
-                user : action.payload
-            }
-        case 'LOGOUT':
-            return{
-                user : null
-            }
-        default :
-            return state
-    }
-})
+export const authReducer = useReducer((state, action) => {
+  switch (action) {
+    case "LOGIN":
+      return {
+        user: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+      };
+    default:
+      return state;
+  }
+});
 
-export const AuthContextProvider = ({Children}) => {
-    const [state, dispatch] = useReducer(authReducer, {user : null})
-}
+export const AuthContextProvider = ({ Children }) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+};
+
+return (
+  <AuthContext.Provider value={{ ...state, action }}>
+    {Children}
+  </AuthContext.Provider>
+);
 
 console.log(state);

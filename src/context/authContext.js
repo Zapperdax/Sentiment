0208de8a373a02e7f637b552fecd21d0ba325197
Ghulam Mujtaba <100614466+Mujtaba-Abbasi@users.noreply.github.com>
2023-01-16 22:23:17 +0500

    add authContext

---
+++ src/context/authContext.js
@@ -0,0 +1,24 @@
+import React, {useReducer, createContext} from 'react';
+
+export const AuthContext = createContext();
+
+export const authReducer = useReducer((state, action)=>{
+    switch(action){
+        case 'LOGIN':
+            return{
+                user : action.payload
+            }
+        case 'LOGOUT':
+            return{
+                user : null
+            }
+        default :
+            return state
+    }
+})
+
+export const AuthContextProvider = ({Children}) => {
+    const [state, dispatch] = useReducer(authReducer, {user : null})
+}
+
+console.log(state);

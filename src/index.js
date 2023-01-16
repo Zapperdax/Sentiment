65c0f56324182ee7e491f9454e2bb84a24cdc84a
Mujtaba-Abbasi <gmabbsi92@gmail.com>
2023-01-16 19:29:51 +0500

    db connection

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-require("./db/mongoose");
-const userRoute = require("./routes/userRoute");
-const otpRoute = require("./routes/otpRoute");
+/** @format */
+
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+import userRoute from "./routes/userRoute.js";
+import otpRoute from "./routes/otpRoute.js";
 
 const app = express();
 
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 app.use(userRoute);
 app.use(otpRoute);
 
-app.listen(port, () => {
-  console.log("Listening on port " + port);
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.MONGODB_URL, () => {
+  app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+    console.log("Database Connected!");
+  });
 });

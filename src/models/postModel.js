36d2706bba3b0a6e3842a4d9cb1bba3ede05089a
Mujtaba-Abbasi <gmabbsi92@gmail.com>
2023-01-16 19:33:13 +0500

    add post model

---
+++ src/models/postModel.js
@@ -0,0 +1,36 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  location: String,
+  description: String,
+  picturePath: String,
+  userPicturePath: String,
+  likes: {
+    type: Map,
+    of: Boolean,
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+});
+
+const Post = mongoose.model("Post", schema);
+
+export default Post;

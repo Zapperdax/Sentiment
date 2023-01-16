10644a9494a80f72570a618ac54898d33759b69d
Mujtaba-Abbasi <gmabbsi92@gmail.com>
2023-01-16 23:17:57 +0500

    add authContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from './App'
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter >
+  </AuthContextProvider>
 );

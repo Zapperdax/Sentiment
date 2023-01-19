a211278e071e73fcc6f24c540573b595ec87e27d
Zapperdax <qa80986@gmail.com>
2023-01-19 10:13:05 +0500

    Fixed faceAPI

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `https://sentiment-backend-muhasim.up.railway.app`,
+  // baseURL: `https://sentiment-backend-muhasim.up.railway.app`,
+  baseURL: `http://localhost:2533`,
 });

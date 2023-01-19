cc8c6808a37d37cf397554c96b39b497df033ced
Mujtaba-Abbasi <gmabbsi92@gmail.com>
2023-01-19 13:19:35 +0500

    add AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from './App'
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/authContext";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(

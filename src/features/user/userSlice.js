8601ff96cb257b05788d8658b17dd6659085d2c9
Zapperdax <qa80986@gmail.com>
2023-03-14 18:00:18 +0500

    Fixed a redux initial state issue

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: localStorage.getItem('user'),
+  user: JSON.parse(localStorage.getItem("user")),
 };
 
 const userSlice = createSlice({

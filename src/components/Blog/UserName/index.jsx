cdb40bb3a3976a68b48eaf87cff4a6d333253468
Mujtaba <gmabbasi92@gmail.com>
2023-05-14 00:16:25 +0500

    code refactor and Blog module

---
+++ src/components/Blog/UserName/index.jsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const UserName = ({ userName }) => {
+  const styles = {
+    fontSize: "16px",
+    fontWeight: "300",
+    color: "#fff",
+    lineHeight: "20px",
+    transform: { xs: "none", md: "rotate(-90deg)" },
+    alignSelf: "flex-start",
+    height: "fit-content",
+    width: "fit-content",
+    position: "relative",
+    left: { xs: "0", md: "35px" },
+  };
+  return <Typography sx={styles}>{userName}</Typography>;
+};
+
+export default UserName;

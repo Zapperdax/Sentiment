a9cf0ce047cc1b7b209ecd4c3c82945f8ce038b5
Mujtaba <gmabbasi92@gmail.com>
2023-05-14 00:15:48 +0500

    code refactor and Blog module

--- src/components/index.js
+++ src/components/index.js
@@ -2,15 +2,10 @@
 
 export { default as ChangePassword } from "./ChangePassword/ChangePassword";
 export { default as Chatbot } from "./Chatbot/Chatbot";
-export { default as ChatbotLandingPage } from "./ChatbotLandingPage/ChatbotLandingPage";
 export { default as EnterOTP } from "./EnterOTP/EnterOTP";
 export { default as FaceDetection } from "./FaceDetection/FaceDetection";
 export { default as ForgotPassword } from "./ForgotPassword/ForgotPassword";
-export { default as Home } from "./HomePage/Home";
-export { default as Login } from "./Login/Login";
 export { default as Navbar } from "./Navbar/Navbar";
-export { default as RegisterPage } from "./RegisterPage/RegisterPage";
-export { default as Blog } from "./Blog/Blog";
 export { default as ProtectRoutes } from "./ProtectedRoutes/ProtectRoutes";
 export { default as MoviesLayout } from "./MoviesLayout/MoviesLayout";
 export { default as MoviesCard } from "./MoviesCard/MoviesCard";

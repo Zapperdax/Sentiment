a7b75d23c7082b530be54fda21cf4f058124b1bd
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-05-14 20:50:51 +0500

    blog post

--- src/components/Blog/PostTitle/index.jsx
+++ src/components/Blog/PostTitle/index.jsx
@@ -1,14 +1,19 @@
 import { Typography } from "@mui/material";
 import React from "react";
 
-const PostTitle = ({ title }) => {
+const PostTitle = ({ title, details }) => {
   const styles = {
-    fontFamily: "DM Serif Display",
-    fontSize: { xs: "24px", md: "32px" },
+    fontFamily: "Eudoxus Sans",
+    fontSize: { xs: "24px", md: "30px" },
     fontWeight: "600",
     lineHeight: { xs: "33px", md: "44px" },
-    color: "#fff",
+    color: "#39DCF3",
   };
+
+  if (details) {
+    styles.fontSize = { xs: "32px", md: "40px" };
+    styles.lineHeight = { xs: "43px", md: "55px" };
+  }
 
   return <Typography sx={styles}>{title}</Typography>;
 };

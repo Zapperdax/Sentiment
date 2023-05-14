1cdeab60615c95aff1cb3f3ee80c452766cf8586
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-05-14 20:50:46 +0500

    blog post

--- src/components/Blog/PostDescription/index.jsx
+++ src/components/Blog/PostDescription/index.jsx
@@ -1,15 +1,44 @@
 import { Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../../constants/navigation";
 
-const PostDescription = ({ description }) => {
+const PostDescription = ({ description, postId = "", details = false }) => {
+  const [showFull, setShowFull] = useState(false);
+
   const styles = {
-    fontWeight: "200",
-    fontSize: { sm: "14px", md: "16px" },
+    fontWeight: "400",
+    fontSize: { sm: "14px", md: "18px" },
     lineHeight: { sm: "20px", md: "27px" },
     // width: "886px",
+    whiteSpace: "pre-line",
+    fontFamily: "Manrope",
   };
 
-  return <Typography sx={styles}>{description}</Typography>;
+  const truncatedDescription =
+    !details && description?.length > 500
+      ? description.substring(0, 500) + "..."
+      : description;
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setShowFull(!showFull);
+  };
+
+  return (
+    <Typography sx={styles}>
+      {truncatedDescription}
+      {!details && description?.length > 444 && (
+        <Link
+          to={`${ROUTES.BLOG_POST.replace(":id", postId)}`}
+          style={{ color: "#39DCF3" }}
+          color="inherit"
+        >
+          {showFull ? "Read less" : "Read more"}
+        </Link>
+      )}
+    </Typography>
+  );
 };
 
 export default PostDescription;

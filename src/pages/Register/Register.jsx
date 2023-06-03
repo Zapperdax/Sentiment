ad2336d4e240545b10db57ae8aa9fe52d9088978
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-06-03 23:35:42 +0500

    UI revamp

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,259 +1,252 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
-import {
-  TextField,
-  InputAdornment,
-  IconButton,
-  Alert,
-  AlertTitle,
-} from "@mui/material";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import { api } from "../../utils/axios";
-import { useDispatch } from "react-redux";
-import { login } from "../../features/user/userSlice";
-
+import { useDispatch, useSelector } from "react-redux";
+import { createUser, login } from "../../features/user/userSlice";
+import FormContainer from "./FormContainer";
+import { registerValidations } from "../../utils/validations/validations";
+import { ROUTES } from "../../constants/navigation";
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 const Register = () => {
-  const innerBackground = "/images/innerBackground3.jpg";
-  const logo = "/images/logo.png";
-
-  const [error, setError] = React.useState(false);
-  const [pClicked, setPClicked] = React.useState(false);
   const navigate = useNavigate();
+  const { loading, error } = useSelector((state) => state.users);
 
   const dispatch = useDispatch();
-
-  const handlePClick = () => {
-    setPClicked((preValue) => !preValue);
-  };
-
-  const [CPClicked, setCPClicked] = React.useState(false);
-  const handleCPClick = () => {
-    setCPClicked((preValue) => !preValue);
-  };
-
-  const [formData, setFormData] = React.useState({
-    userName: "",
-    email: "",
-    password: "",
-    cPassword: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((preValue) => {
-      return {
-        ...preValue,
-        [name]: value,
+  const handleSubmit = async (values) => {
+    try {
+     const res = await dispatch(
+        createUser({
+          name: values.fullName,
+          email: values.email,
+          password: values.password,
+        })
+      );
+      const user = {
+        email: res.payload.data.user.email,
+        token: res.payload.data.token,
       };
-    });
-  };
-
-  // console.log(formData);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    api
-      .post("/newUser", {
-        name: formData.userName,
-        email: formData.email,
-        password: formData.password,
-      })
-      .then((response) => {
-        if (response.status === 201) {
-          const user = {
-            email: response.data.user.email,
-            token: response.data.token,
-          };
-          console.log(user);
-          localStorage.setItem("user", JSON.stringify(user));
-          dispatch(login(user));
-          navigate("/");
-        }
-      })
-      .catch((err) => {
-        setError(true);
-      });
+      localStorage.setItem("user", JSON.stringify(user));
+      await dispatch(login(user));
+      navigate(ROUTES.HOME);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(error)
   };
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-      className="registerForm"
-    >
-      <div
-        style={{
-          minHeight: "80%",
-          width: "72%",
-          display: "grid",
-          gridTemplateColumns: "30% 70%",
-          backgroundImage: `url(${innerBackground})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          borderRadius: "5px",
-          marginTop: "3rem",
-        }}
-        className="registerBox"
-      >
-        <div
-          className="leftSide"
-          style={{
-            minWidth: "100%",
-            minHeight: "100%",
-          }}
-        >
-          <img style={{ margin: "1rem 0 0 1rem" }} src={logo} alt="logo" />
-          <p
-            style={{
-              fontFamily: "Bebas Neue",
-              margin: "1rem 0 0 1rem",
-              color: "white",
-              fontSize: "1.4rem",
-            }}
-          >
-            Sentiment Analysis and Your Mood Companion.
-          </p>
-          <img
-            className="floating"
-            style={{
-              maxWidth: "25%",
-              maxHeight: "50%",
-              position: "absolute",
-              left: "16%",
-              bottom: "15%",
-            }}
-            src="/images/float.png"
-            alt="float"
-          />
-        </div>
-        <div
-          className="rightSide"
-          style={{
-            backgroundColor: "white",
-            borderRadius: "40px 0 0 40px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            // borderRadius: "5px",
-          }}
-        >
-          <div
-            style={{ margin: "3.5rem", width: "100%" }}
-            className="signUpForm"
-          >
-            {error && (
-              <Alert severity="error">
-                <AlertTitle>Error</AlertTitle>
-                This is an error alert — <strong>check it out!</strong>
-              </Alert>
-            )}
-            <div
-              style={{
-                fontSize: "1.5rem",
-                fontFamily: "Bebas Neue",
-                margin: "1rem 0",
-
-                textAlign: "center",
-              }}
-              className="createAccount"
-            >
-              Create Account
-            </div>
-
-            <form className="form">
-              <TextField
-                variant="standard"
-                label="Full Name"
-                fullWidth
-                size="normal"
-                margin="dense"
-                onChange={handleChange}
-                name="userName"
-                value={formData.userName}
-              />
-              <TextField
-                variant="standard"
-                label="Email Address"
-                fullWidth
-                size="normal"
-                margin="dense"
-                onChange={handleChange}
-                name="email"
-                value={formData.email}
-              />
-              <TextField
-                variant="standard"
-                label="Password"
-                fullWidth
-                size="normal"
-                margin="dense"
-                onChange={handleChange}
-                name="password"
-                value={formData.password}
-                type={pClicked ? "text" : "password"}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end" onClick={handlePClick}>
-                      <IconButton>
-                        {pClicked ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <TextField
-                variant="standard"
-                label="Confirm Password"
-                fullWidth
-                size="normal"
-                margin="dense"
-                onChange={handleChange}
-                name="cPassword"
-                value={formData.cPassword}
-                type={CPClicked ? "text" : "password"}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end" onClick={handleCPClick}>
-                      <IconButton>
-                        {CPClicked ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </form>
-            <div style={{ margin: "2rem 0", textAlign: "center" }}>
-              <button onClick={handleSubmit} className="createAccountButton">
-                Create Account
-              </button>
-            </div>
-            <div className="haveAccount">
-              <p
-                style={{
-                  fontFamily: "Playfair Display",
-                  fontWeight: "bold",
-                  color: "#3B3B3B",
-                }}
-              >
-                Already Have An Account?
-                <Link
-                  style={{ textDecoration: "none", color: "#707070" }}
-                  to="/login"
-                >
-                  {" "}
-                  Log in
-                </Link>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <FormContainer
+      loading={loading}
+      type="register"
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      validationSchema={registerValidations}
+    />
   );
 };
 
 export default Register;
+
+
+
+ //   const res = await api.post("/newUser", {
+    //     name: values.fullName,
+    //     email: values.email,
+    //     password: values.password,
+    //   });
+    //   const user = {
+    //     email: res.data.user.email,
+    //     token: res.data.token,
+    //   };
+    //   localStorage.setItem("user", JSON.stringify(user));
+    //   await dispatch(login(user)).finally(() => {
+    //     navigate("/");
+    //   });
+    // } catch (error) {
+    //   console.log(error);
+    //   if (error.response) {
+    //     const { status } = error.response;
+    //     if (status === 400) {
+    //       alert("User already exists");
+    //     } else if (status === 401 || status === 403) {
+    //       alert("You are not authorized to perform this action. Please login or contact support for assistance.");
+    //     } else {
+    //       alert("An unexpected server error occurred. Please try again later.");
+    //     }
+    //   } else {
+    //     alert("Network error occurred. Please check your internet connection.");
+    //   }
+    // }
+{
+  /* <div
+    style={{
+      minHeight: "80%",
+      width: "72%",
+      display: "grid",
+      gridTemplateColumns: "30% 70%",
+      backgroundImage: `url(${innerBackground})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      borderRadius: "5px",
+      marginTop: "3rem",
+    }}
+    className="registerBox"
+  >
+    <div
+      className="leftSide"
+      style={{
+        minWidth: "100%",
+        minHeight: "100%",
+      }}
+    >
+      <img style={{ margin: "1rem 0 0 1rem" }} src={logo} alt="logo" />
+      <p
+        style={{
+          fontFamily: "Bebas Neue",
+          margin: "1rem 0 0 1rem",
+          color: "white",
+          fontSize: "1.4rem",
+        }}
+      >
+        Sentiment Analysis and Your Mood Companion.
+      </p>
+      <img
+        className="floating"
+        style={{
+          maxWidth: "25%",
+          maxHeight: "50%",
+          position: "absolute",
+          left: "16%",
+          bottom: "15%",
+        }}
+        src="/images/float.png"
+        alt="float"
+      />
+    </div>
+    <div
+      className="rightSide"
+      style={{
+        backgroundColor: "white",
+        borderRadius: "40px 0 0 40px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        // borderRadius: "5px",
+      }}
+    >
+      <div
+        style={{ margin: "3.5rem", width: "100%" }}
+        className="signUpForm"
+      >
+        {error && (
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            This is an error alert — <strong>check it out!</strong>
+          </Alert>
+        )}
+        <div
+          style={{
+            fontSize: "1.5rem",
+            fontFamily: "Bebas Neue",
+            margin: "1rem 0",
+
+            textAlign: "center",
+          }}
+          className="createAccount"
+        >
+          Create Account
+        </div>
+
+        <form className="form">
+          <TextField
+            variant="standard"
+            label="Full Name"
+            fullWidth
+            size="normal"
+            margin="dense"
+            onChange={handleChange}
+            name="userName"
+            value={formData.userName}
+          />
+          <TextField
+            variant="standard"
+            label="Email Address"
+            fullWidth
+            size="normal"
+            margin="dense"
+            onChange={handleChange}
+            name="email"
+            value={formData.email}
+          />
+          <TextField
+            variant="standard"
+            label="Password"
+            fullWidth
+            size="normal"
+            margin="dense"
+            onChange={handleChange}
+            name="password"
+            value={formData.password}
+            type={pClicked ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end" onClick={handlePClick}>
+                  <IconButton>
+                    {pClicked ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            variant="standard"
+            label="Confirm Password"
+            fullWidth
+            size="normal"
+            margin="dense"
+            onChange={handleChange}
+            name="cPassword"
+            value={formData.cPassword}
+            type={CPClicked ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end" onClick={handleCPClick}>
+                  <IconButton>
+                    {CPClicked ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </form>
+        <div style={{ margin: "2rem 0", textAlign: "center" }}>
+          <button onClick={handleSubmit} className="createAccountButton">
+            Create Account
+          </button>
+        </div>
+        <div className="haveAccount">
+          <p
+            style={{
+              fontFamily: "Playfair Display",
+              fontWeight: "bold",
+              color: "#3B3B3B",
+            }}
+          >
+            Already Have An Account?
+            <Link
+              style={{ textDecoration: "none", color: "#707070" }}
+              to="/login"
+            >
+              {" "}
+              Log in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div> */
+}
---
+++ src/utils/validations/validations.js
@@ -0,0 +1,32 @@
+import { object, ref, string } from "yup";
+
+const passwordValidations = (requiredMessage = "Password is required.") => {
+  return string()
+    .matches(
+      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+    )
+    .required(requiredMessage);
+};
+
+const emailValidations = (requiredMessage = "Email is required.") => {
+  return string().email("Email is invalid.").required(requiredMessage);
+};
+
+const nameValidations = (requiredMessage = "Name is required.") => {
+  return string().required(requiredMessage);
+};
+export const registerValidations = object().shape({
+  fullName: nameValidations(),
+  email: emailValidations(),
+  password: passwordValidations(),
+  confirmPassword: passwordValidations().oneOf(
+    [ref("password")],
+    "Password must match."
+  ),
+});
+
+export const loginValidations = object().shape({
+  email: emailValidations(),
+  password: string().required("Password is required."),
+});
